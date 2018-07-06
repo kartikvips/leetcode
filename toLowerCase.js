79a1cac7f391982a1c134c2631f67701a724edc5
kartikvips <kartikvips@gmail.com>
2018-07-06 16:24:34 -0600

    toLowerCase

---
+++ toLowerCase.js
@@ -0,0 +1,13 @@
+var toLowerCase = function (str) {
+
+    let out = [];
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt(i);
+        if (code < 91 && code >= 65) {
+            out.push(String.fromCharCode(code + 32));
+        } else {
+            out.push(str[i]);
+        }
+    }
+    return out.join("");
+};
